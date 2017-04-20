137612e40f02dfe26d6c62d0eb05286cf755197a
riyujra <riyujra@bancounion.com.bo>
2017-04-20 10:58:47 -0400

    primer demo

---
+++ app.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('uniKit', []);
+myApp.directive('uniText', function () {
+	return {
+		template: function (elem, attributes) {
+      var data = {
+        id: "text",
+        colSpan: 12,
+        colSpanLabel: 3,
+        colSpanControl: 9
+      };
+      var html = $("<div></div>");
+      var parentNode = elem.parent();
+      for (attr in attributes){
+        if (typeof attributes[attr] === "string" ){
+          data[attr] = attributes[attr];
+        }
+      }
+      _.extend(data, attr.$attr);
+      var tplContent = document.querySelector('#tpl-fieldContent'),
+          tplLabel = document.querySelector('#tpl-fieldlabel'),
+          tplControl = document.querySelector('#tpl-textInput');
+
+      if (tplContent){
+        tplContent = tplContent.innerHTML;
+        content = $(_.template(tplContent, data)).insertBefore(elem);
+      }
+      if (tplLabel){
+        tplLabel = tplLabel.innerHTML;
+        content.append(_.template(tplLabel, data));
+      }
+      if (tplControl){
+        tplControl = tplControl.innerHTML;
+        content.append(_.template(tplControl, data));
+      }
+      elem.remove();
+      //template = document.querySelector('#tpl-text');
+      //if (template){
+        //template = template.innerHTML;
+        //parentNode.append(_.template(template, data));
+        //$(_.template(template, data)).insertBefore(elem);
+        //elem.remove();
+      //}
+      //elem.after(html);
+		}
+	};
+});
+
+/*var myApp = angular.module('uniKit', []);
+myApp.directive("mainCtrl", function(){
+  return {
+    template : function(element, attr){
+          var data = {
+            button: 'button',
+            list: [5,6,7,345,23434,78723,1,2,3,4,5]
+        };  
+        template = document.querySelector('#text').innerHTML;
+        element.html(_.template(template, data));
+      }
+    }
+});*/
