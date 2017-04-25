dc363f157ed0d3476b076a46a22bba009077e606
riyujra <riyujra@bancounion.com.bo>
2017-04-25 10:47:57 -0400

    demo parte2

--- build/app.js
+++ build/app.js
@@ -7,63 +7,61 @@ app.controller('body', function ($scope) {
 app.run(['$rootScope', '$xhrFactory', function ($rootScope, $xhrFactory) {
 		$rootScope.getTpl = function () {
 		};
-		$rootScope.getHTML = function(elem, config){
-			var template, auxHtml = $("div");
+		$rootScope.getHTML = function (elem, config) {
+			var template, auxHtml = $("div"), newTemplate = '<br></br>';
 			type = config.type || elem.get(0).tagName.toLowerCase();
 			switch (type) {
 				case ("grid"):
 					template = document.querySelector('#tpl-grid').innerHTML;
-					//return $(_.template(template, {"colspan": "1", "id": "2"})).insertBefore(elem);
-				 auxHtml.html(_.template(template, {"colspan": config.col, "id": "2"}));
-					//console.log(auxHtml);
-					return auxHtml.get(0).innerHTML;
-					//auxHtml.insertBefore(elem);
+					auxHtml.html(_.template(template, {"colspan": config.col, "id": "2"}));
+					newTemplate = auxHtml.get(0).innerHTML;
 					break;
 				case ("label"):
 					template = document.querySelector('#tpl-fieldlabel').innerHTML;
-					return _.template(template, {"colspan": config.col});
+					newTemplate = _.template(template, {"colspan": config.col});
 					break;
 				case ("input"):
 					template = document.querySelector('#tpl-textInput').innerHTML;
 					return _.template(template, {"colspan": config.col});
-				 //_.template(template, {"colspan": config.col});
-					//return auxHtml.get(0).innerHTML;
 					break;
 			}
+			return newTemplate;
 		};
 	}]);
 
 app.directive('uniAspect', ['$rootScope', function ($rootScope) {
 		var getHTML = $rootScope.getHTML;
-		var config,
-			html,
-			parentConfig,
-			contCols = 0,
-			contElements = 0;
+		var defaultItem = {col: 3}, defaultGrid = {cols: [3, 3, 3, 3], col: 12},
+										html,
+										parentConfig,
+										contCols = 0,
+										contElements = 0,
+										contBR = 0;
 		return {
 			restrict: 'A',
 			transclude: true,
 			replace: true,
 			template: function (elem, attributes) {
-				config = {};
-				if (attributes.uniAspect){
-					config = JSON.parse(attributes.uniAspect);
+				var tagName = elem.get(0).tagName;
+				var settings = {}, pos;
+
+				if (attributes.uniAspect) {
+					settings = JSON.parse(attributes.uniAspect);
 				}
-				if(config.type === "grid"){
-					//elem.children().data({"parentConfig": config});
-					parentConfig = config;
+				if (settings.type === "grid") {
+					_.extend(defaultGrid, settings);
 				} else {
-					if (!config.col){
-							config.col = parentConfig.cols[cont];
+					if(tagName !== "BR"){
+					if (!settings.col) {
+						pos = (elem.index()-contBR) % defaultGrid.cols.length;
+						settings.col = defaultGrid.cols[pos];
+					}						
+					}else{
+						contBR+=1;
 					}
-					contCols = contCols + config.col;
 				}
-				html = getHTML(elem, config);
-				console.log(html);
-				return html;					
-				//elem.remove();
-				//return html;
-				//console.log(elem)
+				html = getHTML(elem, settings);
+				return html;
 			}
 		};
 	}]);
